50815857a8ab613ac7e66157ec19f4d1ea43bb56
Bill Moorby <billmoorby@gmail.com>
2026-02-09 15:03:44 -0800

    Make the cart quantity interactive

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -136,6 +136,14 @@ document.querySelectorAll('.js-add-to-cart')
         });
       };
 
-      console.log(cart);
+      // Calculate total cart quantity
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
     });
 });
