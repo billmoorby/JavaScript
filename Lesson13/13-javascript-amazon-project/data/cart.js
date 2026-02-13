2161d954f3bbd8eda3709f1776deb2a9b66599c2
Bill Moorby <billmoorby@gmail.com>
2026-02-13 12:59:47 -0800

    Save cart in localStorage

--- Lesson13/13-javascript-amazon-project/data/cart.js
+++ Lesson13/13-javascript-amazon-project/data/cart.js
@@ -1,4 +1,9 @@
-export let cart = [
+// if (!cart) {
+
+// }
+
+export let cart = JSON.parse(localStorage.getItem('cart')) ||
+[
   {
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 
     quantity: 2
@@ -9,6 +14,11 @@ export let cart = [
     quantity: 1
   }
 ];
+
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+};
 
 
 export function addToCart(productId) {
@@ -35,6 +45,8 @@ export function addToCart(productId) {
       quantity
     });
   };
+
+  saveToStorage();
 };
 
 
@@ -48,4 +60,6 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+
+  saveToStorage();
 };
