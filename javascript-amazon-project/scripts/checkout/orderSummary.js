90be27baad55255f013c6243dcc7562b50aa9d66
Bill Moorby <billmoorby@gmail.com>
2026-02-19 23:28:42 -0800

    Make payment summary interactive

--- javascript-amazon-project/scripts/checkout/orderSummary.js
+++ javascript-amazon-project/scripts/checkout/orderSummary.js
@@ -4,12 +4,8 @@ import {formatCurrency} from '../utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
+import {renderPaymentSummary} from './paymentSummary.js';
 
-hello();
-
-const today  = dayjs();
-const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
 
 export function renderOrderSummary() {
 
@@ -131,6 +127,9 @@ export function renderOrderSummary() {
         const container = document.querySelector(`.js-cart-item-container-${productId}`);
 
         container.remove();
+
+        // Update the data(model) and regenerate the HTML
+        renderPaymentSummary();
       });
   });
 
@@ -140,7 +139,9 @@ export function renderOrderSummary() {
         const {productId, deliveryOptionId} = element.dataset;
         updateDeliveryOption(productId, deliveryOptionId);
 
+        // Update the data(model) and regenerate the HTML
         renderOrderSummary();
+        renderPaymentSummary();
       });
     });
   };
