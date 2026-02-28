fe3bee4e6a969dd4a88c6bca66289829f8a54a00
Bill Moorby <billmoorby@gmail.com>
2026-02-28 13:05:25 -0800

    16h. Check that product prices are correctly displayed.

--- javascript-amazon-project/tests/checkout/orderSummaryTest.js
+++ javascript-amazon-project/tests/checkout/orderSummaryTest.js
@@ -59,6 +59,15 @@ describe('test suite: renderOrderSummary', () => {
     expect(
       document.querySelector(`.js-product-name-${productId2}`).innerText
     ).toEqual('Intermediate Size Basketball');
+
+    // Check that product prices are correctly displayed.
+    expect(
+      document.querySelector(`.js-product-price-${productId1}`).innerText
+    ).toEqual('$10.90');
+
+    expect(
+      document.querySelector(`.js-product-price-${productId2}`).innerText
+    ).toEqual('$20.95');
   });
 
   // Check delete links work
@@ -89,5 +98,10 @@ describe('test suite: renderOrderSummary', () => {
     expect(
       document.querySelector(`.js-product-name-${productId2}`).innerText
     ).toEqual('Intermediate Size Basketball');
+
+    // Check that product prices are correctly displayed.
+    expect(
+      document.querySelector(`.js-product-price-${productId2}`).innerText
+    ).toEqual('$20.95');
   });
 });
