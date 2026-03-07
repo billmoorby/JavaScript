530741794157d36236f7d2603c460a4e0b7f32af
Bill Moorby <billmoorby@gmail.com>
2026-03-07 10:49:40 -0800

    17c

--- Lesson17/data/car.js
+++ Lesson17/data/car.js
@@ -1,6 +1,7 @@
 class Car {
   brand;
   model;
+  speed = 0;
 
   constructor(carDetails) {
     this.brand = carDetails.brand;
@@ -9,8 +10,28 @@ class Car {
 
   // displayInfo method
   displayInfo() {
-    console.log(`${this.brand} ${this.model}`);
+    console.log(`${this.brand} ${this.model}, Speed: ${this.speed} km/h`);
   }
+
+  // Increases speed by 5
+  go() {
+    this.speed += 5;
+
+    // Limit the speed to 200
+    if (this.speed > 200) {
+      this.speed = 200;
+    };
+  }
+
+  // Decreases speed by 5
+  brake() {
+    this.speed -= 5;
+
+    // Limit the speed to 0.
+    if (this.speed < 0) {
+      this.speed = 0;
+    };
+  };
 }
 
 const car1 = new Car({
@@ -27,4 +48,14 @@ console.log(car1);
 console.log(car2);
 
 car1.displayInfo();
+car1.go();
+car1.go();
+car1.go();
+car1.brake();
+car1.displayInfo();
+
+car2.displayInfo();
+car2.go();
+car2.brake();
+car2.brake();
 car2.displayInfo();
