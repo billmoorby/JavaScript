083ffc49e1156f513dcc9ed153c7a5564612ccb6
Bill Moorby <billmoorby@gmail.com>
2026-03-10 13:36:23 -0700

    Learn promises and convert code to promises.

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,10 +1,62 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import {loadProducts} from '../data/products.js';
+import {loadCart} from '../data/cart.js';
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
-loadProducts(() => {
+// Promises = better way to handle asynchronous code. Similar to done(). lets us wait for code to finish before moving to next step. Helps avoid nesting and keep code flat.
+
+// promise.all() lets us run multiple promises at the SAME TIME, and wait for all of them to finish. Use an array of promises
+Promise.all([
+  new Promise((resolve) => {
+  loadProducts(() => {
+    resolve('value1');
+    });
+  }), 
+
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+
+]).then((values) => {
+  console.log(values);
   renderOrderSummary();
   renderPaymentSummary();
 });
+
+/*
+new Promise((resolve) => {
+  loadProducts(() => {
+    // resolve parameter = function that lets us control when to go to the next step.
+    resolve('value1');
+  })
+
+// Does next step(then()) in SEPERATE THREAD independent of the other code.
+}).then((value) => {
+  console.log(value);
+
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+
+}).then(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+*/
+
+
+// Multiple callbacks = a lot of nesting(code inside code).
+/*
+loadProducts(() => {
+  loadCart(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+});
+*/
