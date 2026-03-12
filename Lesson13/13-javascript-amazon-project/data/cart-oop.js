14e8d7e86e9c075486df3e4e0148095700d252a9
Bill Moorby <billmoorby@gmail.com>
2026-03-12 16:25:54 -0700

    Create oop version of cart.

---
+++ Lesson13/13-javascript-amazon-project/data/cart-oop.js
@@ -0,0 +1,136 @@
+import {validDeliveryOption} from './deliveryOptions.js';
+
+// Object Oriented Programming uses objects to represent the real world. Easy to create multiple objects.
+
+// Function to generate Cart objects.
+function Cart(localStorageKey) {
+  // Creating a cart object.
+  const cart = {
+    cartItems: undefined, 
+
+    loadFromStorage() {
+      this.cartItems = JSON.parse(localStorage.getItem(localStorageKey)) ||
+      [
+        {
+          productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 
+          quantity: 2, 
+          deliveryOptionId: '1'
+        },
+
+        {
+          productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d', 
+          quantity: 1, 
+          deliveryOptionId: '2'
+        }
+      ];
+    }, 
+
+    saveToStorage() {
+      localStorage.setItem(localStorageKey, JSON.stringify(this.cartItems));
+    }, 
+
+    addToCart(productId, quantity) {
+      let matchingItem;
+
+      // Check for duplicate items so we can increase quantity
+      this.cartItems.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+          matchingItem = cartItem;
+        }
+      });
+
+      // 13c
+      // const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+
+      // 13d
+      // const quantity = Number(quantitySelector.value);
+
+      if (matchingItem) {
+        // matchingItem.quantity += 1;
+        matchingItem.quantity += quantity;
+      } else {
+        this.cartItems.push({
+          productId, 
+          quantity, 
+          deliveryOptionId: '1'
+        });
+      };
+
+      this.saveToStorage();
+    }, 
+
+    removeFromCart(productId) {
+      const newCart = [];
+
+      this.cartItems.forEach((cartItem) => {
+        if (cartItem.productId !== productId) {
+          newCart.push(cartItem);
+        };
+      });
+
+      this.cartItems = newCart;
+
+      this.saveToStorage();
+    }, 
+
+    calculateCartQuantity() {
+      let cartQuantity = 0;
+
+      this.cartItems.forEach((cartItem) => {
+        cartQuantity += cartItem.quantity;
+      });
+
+      return cartQuantity;
+    }, 
+
+    updateQuantity(productId, newQuantity) {
+      let matchingItem;
+
+      this.cartItems.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+          matchingItem = cartItem;
+        };
+      });
+
+      matchingItem.quantity = newQuantity;
+
+      this.saveToStorage();
+    }, 
+
+    updateDeliveryOption(productId, deliveryOptionId) {
+      let matchingItem;
+
+      this.cartItems.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+          matchingItem = cartItem;
+        };
+      });
+
+      // Case where we have an invalid productId in the cart(we cannot update its delivery option), do nothing.
+      if (!matchingItem) {
+        return;
+      };
+
+      // Case where we have an invalid deliveryOptionId. Do nothing.
+      if (!validDeliveryOption(deliveryOptionId)) {
+        return;
+      };
+
+      matchingItem.deliveryOptionId = deliveryOptionId;
+
+      this.saveToStorage();
+    }
+  };
+
+  return cart;
+};
+
+const cart = new Cart('cart-oop');
+const businessCart = new Cart('cart-business');
+
+cart.loadFromStorage();
+
+businessCart.loadFromStorage();
+
+console.log(cart);
+console.log(businessCart);
