32120e36f1026d1b4d9e6aeaad770c74ed24edfa
Bill Moorby <billmoorby@gmail.com>
2026-03-12 13:55:47 -0700

    16l. Updated updateDeliveryOption() to handle invalid productId NOT in the cart. Created specs for invalid productId edge case.

--- Lesson13/13-javascript-amazon-project/data/cart.js
+++ Lesson13/13-javascript-amazon-project/data/cart.js
@@ -107,6 +107,11 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
     };
   });
 
+  // Case where we have an invalid productId in the cart(we cannot update its delivery option), do nothing.
+  if (!matchingItem) {
+    return;
+  };
+
   matchingItem.deliveryOptionId = deliveryOptionId;
 
   saveToStorage();
