65e50f0ec3e140dfcf9b070511c3f89da848ddf9
Bill Moorby <billmoorby@gmail.com>
2026-03-12 11:47:35 -0700

    16f. afterEach hook to clear HTML after each spec.

--- Lesson13/13-javascript-amazon-project/tests/checkout/orderSummaryTest.js
+++ Lesson13/13-javascript-amazon-project/tests/checkout/orderSummaryTest.js
@@ -35,6 +35,12 @@ describe('test suite: renderOrderSummary', () => {
     renderOrderSummary();
   });
 
+  // afterEach hook to cleanup the HTML after each spec.
+  afterEach(() => {
+    // Clear the HTML
+    document.querySelector('.js-test-container').innerHTML = '';
+  });
+
   it('displays the cart', () => {
     expect(
       document.querySelectorAll('.js-cart-item-container').length
@@ -47,9 +53,6 @@ describe('test suite: renderOrderSummary', () => {
     expect(
       document.querySelector(`.js-product-quantity-${productId2}`).innerText
     ).toContain('Quantity: 1');
-
-    // Clear the HTML
-    document.querySelector('.js-test-container').innerHTML = '';
   });
 
 
@@ -74,10 +77,7 @@ describe('test suite: renderOrderSummary', () => {
 
     // Is cart array updated.
     expect(cart.length).toEqual(1);
-    expect(cart[0].productId).toEqual(productId2);
-
-    // Clear the HTML
-    document.querySelector('.js-test-container').innerHTML = '';
+    expect(cart[0].productId).toEqual(productId2);    
   });
 });
 
