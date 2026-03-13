c004db40c22fb8a12559606deacc7fed026b533a
Bill Moorby <billmoorby@gmail.com>
2026-03-13 15:53:59 -0700

    Learn more details about classes and this.

--- Lesson13/13-javascript-amazon-project/data/products.js
+++ Lesson13/13-javascript-amazon-project/data/products.js
@@ -64,6 +64,42 @@ class Clothing extends Product {
   }
 }
 
+/*
+const date = new Date();
+console.log(date);
+console.log(date.toLocaleTimeString());
+*/
+
+/*
+// No object for "this" to point to, so it will be undefined.
+console.log(this);
+
+
+// No object for "this" to point to, so it will be undefined since object has NOT been created yet.
+const object2 = {
+  a: 2, 
+  b: this.a
+};
+*/
+
+/*
+// No object for "this" to point to, so it will be undefined.
+function logThis() {
+  console.log(this);
+};
+logThis();
+// call() sets value of "this". Prints 'hello'
+logThis.call('hello');
+
+// Arrow functions do NOT change the value of 'this'.
+const object3 = {
+  method: () => {
+    console.log(this);
+  }
+};
+object3.method();
+*/
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
