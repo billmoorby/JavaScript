06bd385e2c08192d221e8ffdd785bfe8fe7cb7ea
Bill Moorby <billmoorby@gmail.com>
2026-03-14 12:56:38 -0700

    Learn promises and convert code to promises.

--- Lesson13/13-javascript-amazon-project/scripts/checkout.js
+++ Lesson13/13-javascript-amazon-project/scripts/checkout.js
@@ -2,19 +2,67 @@ import {renderCheckoutHeader} from './checkout/checkoutHeader.js';
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import {loadProducts} from '../data/products.js';
+import {loadCart} from '../data/cart.js';
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
-loadProducts(() => {
+
+// Promises = better way to handle asynchronous code. Similar to done(). Lets us wait for code to finish before moving to next step Helps avoid nesting and keeps code flat.
+
+
+// promise.all() lets us run multiple promises at the SAME TIME, nad wait for all of them to finish. Use an array of promises.
+Promise.all([
+  new Promise((resolve) => {
+  loadProducts(() => {
+    resolve('value1');
+    });
+  }), 
+
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+
+]).then((values) => {
+  console.log(values);
   renderCheckoutHeader();
   renderOrderSummary();
   renderPaymentSummary();
 });
 
+
 /*
-document.addEventListener('DOMContentLoaded', () => {
+new Promise((resolve) => {
+  // resolve parameter = function that lets us control when we go to the next step.
+  loadProducts(() => {
+    resolve('value1');
+  });
+
+  // Does next step(then()) in SEPARATE THREAD independent of the other code.
+}).then((value) => {
+  console.log(value);
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+
+}).then(() => {
   renderCheckoutHeader();
   renderOrderSummary();
   renderPaymentSummary();
+});
+*/
+
+
+// Multiple callbacks = a lot of nesting(code inside code).
+/*
+loadProducts(() => {
+  loadCart(() => {
+    renderCheckoutHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
 });
 */
